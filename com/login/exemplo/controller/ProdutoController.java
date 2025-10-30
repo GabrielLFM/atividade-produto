e5d5082bc315e6819152976cdf300cabb4418b53
GabrielLFM <biel.lucianomelo@gmail.com>
2025-10-30 11:04:20 -0300

    Product table

---
+++ com/login/exemplo/controller/ProdutoController.java
@@ -0,0 +1,77 @@
+package com.login.exemplo.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.login.exemplo.entity.Produto;
+import com.login.exemplo.repositories.ProdutoRepository;
+
+@RestController
+@RequestMapping("produto")
+public class ProdutoController {
+
+	@Autowired
+	ProdutoRepository produtoRepository;
+	
+	@GetMapping("{id}")
+	public Optional<Produto> produtoPorId(@PathVariable int id){
+		return produtoRepository.findById(id) ;
+	}
+	
+	@PostMapping(value = "/cadastrar")
+	public ResponseEntity<?> saveUser (@RequestBody Produto prod){
+		Produto produto = new Produto(prod.getNome(), prod.getPreco(), prod.getQuantidade());
+		produtoRepository.save(produto);
+		return ResponseEntity.ok("O usuário foi cadastrado com sucesso!");
+	}
+	
+	
+	@GetMapping(value = "buscar")
+	public List<Produto>ListarProduto1(){
+		List<Produto> listaDeProdutos = produtoRepository.findAll();
+		return listaDeProdutos;
+	}
+	
+	@GetMapping(value = "buscar2")
+	public List<Produto> listarProduto(){
+		return produtoRepository.findAll();
+	}
+	
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<?> deletar(@PathVariable int id){
+		if (produtoRepository.existsById(id)) {
+			produtoRepository.deleteById(id);
+			return ResponseEntity.status(HttpStatus.OK).body("Excluido com Sucesso!"); // 204
+		}else {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Esse Id não existe"); // 404
+		}
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<Produto> atualizar(@PathVariable int id, @RequestBody Produto novaQuantidade){
+		Optional<Produto> ProdutoExistente = produtoRepository.findById(id);
+		
+		if(ProdutoExistente.isPresent()) {
+			Produto Produto = ProdutoExistente.get();
+			Produto.setQuantidade(novaQuantidade.getQuantidade());
+			produtoRepository.save(Produto);
+			return ResponseEntity.ok(Produto);
+		} else {
+			return ResponseEntity.notFound().build();
+		}
+	}
+
+}
---
+++ com/login/exemplo/controller/UsuarioController.java
@@ -0,0 +1,93 @@
+package com.login.exemplo.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.login.exemplo.entity.Usuario;
+import com.login.exemplo.repositories.UsuarioRepository;
+
+@RestController
+@CrossOrigin(origins = "*")
+public class UsuarioController {
+
+		@Autowired
+		UsuarioRepository usuarioRepository;
+		
+		@PostMapping(value = "usuario/cadastro")
+		public ResponseEntity<?> saveUser (@RequestBody Usuario user){
+			Usuario usuario = new Usuario(user.getName(), user.getEmail(), user.getPassword());
+			usuarioRepository.save(usuario);
+			return ResponseEntity.ok("O usuário foi cadastrado com sucesso!");
+		}
+		
+		@PostMapping(value = "login")
+		public ResponseEntity<?> login(@RequestBody Usuario user){
+			
+			Usuario findUser = usuarioRepository.findByEmail(user.getEmail());
+			if (findUser == null) {
+				return ResponseEntity.ok("Usuário não encontrado");
+			}else {
+				if (findUser.getPassword().equals(user.getPassword())) {
+					return ResponseEntity.ok("Usuário Logado!");
+					}else {
+						return ResponseEntity.ok("Senha incorrreta");
+					}
+			}
+		}
+		
+		@GetMapping(value = "Fiama")
+		public List<Usuario>ListarUsuarios1(){
+			List<Usuario> listaDeUsuarios = usuarioRepository.findAll();
+			return listaDeUsuarios;
+		}
+		
+		@GetMapping(value = "Luciano")
+		public List<Usuario> listarUsuario(){
+			return usuarioRepository.findAll();
+		}
+		
+		@GetMapping(value = "{id}")
+		public Optional<Usuario> usuarioPorId(@PathVariable int id){
+			return usuarioRepository.findById(id) ;
+		}
+		
+		@DeleteMapping("/{id}")
+		public ResponseEntity<?> deletar(@PathVariable int id){
+			if (usuarioRepository.existsById(id)) {
+				usuarioRepository.deleteById(id);
+				return ResponseEntity.status(HttpStatus.OK).body("Excluido com Sucesso!"); // 204
+			}else {
+				return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Esse Id não existe"); // 404
+			}
+		}
+		
+		@PutMapping("/{id}")
+		public ResponseEntity<Usuario> atualizar(@PathVariable int id, @RequestBody Usuario novoUsuario){
+			Optional<Usuario> UsuarioExistente = usuarioRepository.findById(id);
+			
+			if(UsuarioExistente.isPresent()) {
+				Usuario Usuario = UsuarioExistente.get();
+				Usuario.setName(novoUsuario.getName());
+				Usuario.setPassword(novoUsuario.getPassword());
+				usuarioRepository.save(Usuario);
+				return ResponseEntity.ok(Usuario);
+			} else {
+				return ResponseEntity.notFound().build();
+			}
+		}
+		
+	}
+
+
